615c4c3ac9835e9aff93a06e00d04e37a4455e1c
Paulo Victor <paulovictordev16@gmail.com>
2025-12-27 19:20:34 -0300

    feat(App): add Footer component to main application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Router from './Router'
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Router />
+      <Footer />
     </>
   )
 }
