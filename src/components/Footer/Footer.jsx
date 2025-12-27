b614ca9fec7ed0021cba0b8f2b11aeec871209ab
Paulo Victor <paulovictordev16@gmail.com>
2025-12-27 00:23:26 -0300

    feat(Footer): add Footer component with styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        The Simpsons Wiki - created by{' '}
+        <a
+          href="https://www.linkedin.com/in/paulopbi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Paulo Victor
+        </a>
+        .
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
