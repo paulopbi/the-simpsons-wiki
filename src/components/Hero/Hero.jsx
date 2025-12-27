dd35e2f21c763e10f8ddfd8d84a23f57ee672884
Paulo Victor <paulovictordev16@gmail.com>
2025-12-26 21:52:39 -0300

    feat: add hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <section className="container hero">
+      <h1 className="hero__title">Popular Characters</h1>
+      <p className="hero__text">
+        Meet some of the most iconic characters from Springfield.
+      </p>
+    </section>
+  )
+}
+
+export default Hero
