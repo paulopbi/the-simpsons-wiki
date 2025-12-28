35af2141ec4c42c1a0ce1932632865107e3a0188
Paulo Victor <paulovictordev16@gmail.com>
2025-12-28 15:58:14 -0300

    refactor(CharacterLayout): update image container class and enhance image hover effect

--- src/layout/Character/CharacterLayout.jsx
+++ src/layout/Character/CharacterLayout.jsx
@@ -1,62 +1,89 @@
+import './CharacterLayout.css'
+
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { fetchCharacterById } from '../../services/api'
-import './CharacterLayout.css'
+
 import Badge from '../../components/Badge/Badge'
+import Error from '../../components/Alert/Alert'
+import Alert from '../../components/Alert/Alert'
+import Loading from '../../components/Loading/Loading'
 
 function CharacterLayout() {
   const { id } = useParams()
-  const [character, setCharacter] = useState({})
+  const [characterData, setCharacterData] = useState({})
+
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const { occupation, name, description, gender, age, portrait_path, status } =
-    character
+    characterData
 
   useEffect(() => {
-    const fetchCharacter = async () => {
-      const data = await fetchCharacterById(id)
-      setCharacter(data)
+    const fetchCharacters = async () => {
+      setLoading(true)
+      setError('')
+
+      try {
+        const request = await fetch(
+          `https://thesimpsonsapi.com/api/characters/${id}`
+        )
+
+        if (!request.ok) {
+          throw new Error('Something went wrong, please try again!')
+        }
+
+        const response = await request.json()
+        setCharacterData(response)
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error)
+          setError(error.message)
+        }
+        setCharacterData(null)
+      } finally {
+        setLoading(false)
+      }
     }
 
-    fetchCharacter()
+    fetchCharacters()
   }, [])
 
-  console.log(character)
   return (
-    <>
-      {character ? (
-        <section className="container">
-          <article className="character">
-            <div className="character__img-container">
-              <img
-                src={`https://cdn.thesimpsonsapi.com/1280${portrait_path}`}
-                alt={name}
-                title={`Portrait of ${name}`}
-                loading="lazy"
-              />
+    <section className="container">
+      {loading && <Loading />}
+
+      {error ? (
+        <Alert type="danger">{error}</Alert>
+      ) : (
+        <article className="character">
+          <div className="character__img">
+            <img
+              src={`https://cdn.thesimpsonsapi.com/1280${portrait_path}`}
+              alt={name}
+              title={`Portrait of ${name}`}
+              loading="lazy"
+            />
+          </div>
+
+          <span className="character__id"># {id}</span>
+
+          <section className="character__header">
+            <div className="character__info">
+              <h2>{name}</h2>
+              <sup>{occupation}</sup>
             </div>
 
-            <span className="character__id"># {id}</span>
+            <div className="character__badges">
+              <Badge>Age: {age ? age : 'Unknow'}</Badge>
+              <Badge>Gender: {gender ? gender : 'Unknow'}</Badge>
+              <Badge>Status: {status ? status : 'Unknow'}</Badge>
+            </div>
+          </section>
 
-            <section className="character__header">
-              <div className="character__info">
-                <h2>{name}</h2>
-                <sup className="character__occupation">{occupation}</sup>
-              </div>
-
-              <div className="character__badges">
-                <Badge>Age: {age ? age : 'Unknow'}</Badge>
-                <Badge>Gender: {gender ? gender : 'Unknow'}</Badge>
-                <Badge>Status: {status ? status : 'Unknow'}</Badge>
-              </div>
-            </section>
-
-            <p className="character__description">{description}</p>
-          </article>
-        </section>
-      ) : (
-        <p>Characters not found</p>
+          <p className="character__description">{description}</p>
+        </article>
       )}
-    </>
+    </section>
   )
 }
 
